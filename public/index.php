0e6fede1af1c72a8eef931ecf20b7d514502341d
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2025-11-28 00:48:06 -0300

    feat: add htaccess and update index in public

--- public/index.php
+++ public/index.php
@@ -1,19 +1,14 @@
 <?php
 
+// Carga de las dependencias
 require_once __DIR__ . "/../vendor/autoload.php";
 
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+// Configuración ante cors
+require_once __DIR__ . "/../config/cors-control.php";
 
 require_once __DIR__ . "/../config/config.php";
 
+// Configuración de rutas
 require_once __DIR__ . "/../routes/api.php";
 
 Flight::start();
