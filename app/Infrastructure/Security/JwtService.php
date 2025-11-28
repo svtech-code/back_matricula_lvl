e03550c25413a4b63c1094779ac979fc2130228b
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2025-11-28 01:19:27 -0300

    feat: auth and jwt

--- app/Infrastructure/Security/JwtService.php
+++ app/Infrastructure/Security/JwtService.php
@@ -40,18 +40,4 @@ class JwtService
             return null;
         }
     }
-
-    public function getTokenFromHeader(): ?string
-    {
-        $headers = getallheaders();
-        
-        if (isset($headers['Authorization'])) {
-            $authHeader = $headers['Authorization'];
-            if (preg_match('/Bearer\s+(.*)$/i', $authHeader, $matches)) {
-                return $matches[1];
-            }
-        }
-        
-        return null;
-    }
 }
---
+++ app/Presentation/Middleware/AuthMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Presentation\Middleware;
+
+class AuthMiddleware
+{
+    public static function getTokenFromHeader(): ?string
+    {
+        $headers = getallheaders();
+        
+        if (isset($headers['Authorization'])) {
+            $authHeader = $headers['Authorization'];
+            if (preg_match('/Bearer\s+(.*)$/i', $authHeader, $matches)) {
+                return $matches[1];
+            }
+        }
+        
+        return null;
+    }
+}
