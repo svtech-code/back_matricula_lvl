ca546ad824de0c8eeadcf7f5631c7659ccb6633d
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2025-12-08 12:55:58 -0300

    fix: add new method http patch

--- config/cors-control.php
+++ config/cors-control.php
@@ -1,7 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
